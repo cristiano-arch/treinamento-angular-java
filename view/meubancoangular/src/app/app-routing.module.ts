f5d1f72465fa8dc73410b41921bf9ae49b33d3ca
Cristiano Jr <cristianoandre96@gmail.com>
2021-11-28 17:35:38 -0200

    Primeiro commit: Adicionando README.md aos outros diretorios

---
+++ view/meubancoangular/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router';
+import { DepositoComponent } from './pages/deposito/deposito.component';
+import { ExtratoComponent } from './pages/extrato/extrato.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SaqueComponent } from './pages/saque/saque.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'extrato', component: ExtratoComponent },
+  { path: 'saque', component: SaqueComponent },
+  { path: 'deposito', component: DepositoComponent },
+];
+
+@NgModule({
+
+  imports: [RouterModule.forRoot(routes)],
+
+  exports: [RouterModule]
+
+})
+
+export class AppRoutingModule { }
